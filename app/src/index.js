737782864c67a0691ea66457295ad86dd82565f6
zimjoe <zimjoe@gmail.com>
2023-01-12 23:27:49 -0500

    Branding?

--- app/src/index.js
+++ app/src/index.js
@@ -9,13 +9,17 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <header className="App-header">
-      <h1>QR Code Generator</h1>
+      <h1>Qrap.io</h1>
+      <h2><span>Crappio</span> The Internet QR Code Generator</h2>
     </header>
     <App />
+    <footer>
+      <div>Aeveco 2023</div>
+    </footer>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
