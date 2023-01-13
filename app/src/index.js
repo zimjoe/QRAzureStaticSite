22049a45b5126daa1201f1b0a377ddc72ff0b333
zimjoe <zimjoe@gmail.com>
2023-01-12 23:38:51 -0500

    commented out the reportwebvitals.

--- app/src/index.js
+++ app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 //import Header from './Header';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
