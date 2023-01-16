d9193d25a491372c88a6dd09e67708d509f16364
zimjoe <zimjoe@gmail.com>
2023-01-16 10:41:26 -0500

    added a little clarity to the hinky Menu and changed links to the Wiki pages

--- app/src/index.js
+++ app/src/index.js
@@ -7,11 +7,18 @@ import App from './App';
 
 // hinky solution for now.  Will fix later with a proper menu
 const toggleMenu = (show, hide)=>{
-  let showForm = document.getElementById(show);
-  let hideForm = document.getElementById(hide);
+  
+  let showForm = document.getElementById(show + "Form");
+  let hideForm = document.getElementById(hide + "Form");
   if(showForm && hideForm){
     showForm.className = "";
     hideForm.className = "hidden";
+  }
+  let showButton = document.getElementById("Nav" + show);
+  let hideButton = document.getElementById("Nav" + hide);
+  if(showButton && hideButton){
+    hideButton.className = "";
+    showButton.className = "current";
   }
 };
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -19,9 +26,10 @@ root.render(
   <React.StrictMode>
     
     <nav>
-      <button onClick={()=>{toggleMenu("UrlForm", "WifiForm")}}>Url QR</button>
-      <button onClick={()=>{toggleMenu("WifiForm", "UrlForm")}}>Wifi QR</button>
+      <button id="NavUrl" className='current' onClick={()=>{toggleMenu("Url", "Wifi")}}>Make a Url QR</button>
+      <button id="NavWifi" onClick={()=>{toggleMenu("Wifi", "Url")}}>Make a Wifi QR</button>
     </nav>
+
     <App />
     
   </React.StrictMode>
