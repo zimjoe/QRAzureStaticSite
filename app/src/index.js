54968c02e740c3e8938bd8d7f148796fa0118439
zimjoe <zimjoe@gmail.com>
2023-01-16 10:31:54 -0500

    moved some of the static content to the index.html to help with discoverability

--- app/src/index.js
+++ app/src/index.js
@@ -17,27 +17,13 @@ const toggleMenu = (show, hide)=>{
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <header className="header row">
-      <h1>Qrap.io</h1>
-      <h2><span>Qrapio</span>QR Code Generator</h2>
-    </header>
+    
     <nav>
       <button onClick={()=>{toggleMenu("UrlForm", "WifiForm")}}>Url QR</button>
       <button onClick={()=>{toggleMenu("WifiForm", "UrlForm")}}>Wifi QR</button>
     </nav>
     <App />
-    <footer>
-      <div>An open source project for deploying a QR Azure App.</div>
-      <ul>
-        <li>
-          <a href='https://github.com/zimjoe/QRAzureStaticSite'>https://github.com/zimjoe/QRAzureStaticSite</a>
-        </li>
-        <li>
-          <a href='https://github.com/zimjoe/QRAzureFunctions'>https://github.com/zimjoe/QRAzureFunctions</a>
-        </li>
-      </ul>
-      <div className='footer_aeveco'>Aeveco 2023</div>
-    </footer>
+    
   </React.StrictMode>
 );
 
